3d97b4ed0dcbb1c6444b23e17c1327d12bfc7538
Syed Murtaza Rizvi <Syedmurtazarazavee@gmail.com>
2026-08-08 19:01:05 +0530

    chore(ci): move asset generation to build-android.yml
    Consolidate icon and splash generation into the Android build workflow to simplify CI pipelines and remove redundant scripts.

--- fix.sh
+++
@@ -1,6 +0,0 @@
-npm install sharp
-node -e "
-const sharp = require('sharp');
-sharp('assets/icon.svg').resize(1024, 1024).png().toFile('assets/icon.png');
-sharp('assets/splash.svg').resize(2732, 2732).png().toFile('assets/splash.png');
-"
